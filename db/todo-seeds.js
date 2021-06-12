5a1b2516477e00c4ecd9e3621b9a342a4f84d1ff
trevor4n <trevor4n@gmail.com>
2021-06-12 11:51:28 -0500

    heroku initially seeded

--- db/todo-seeds.js
+++ db/todo-seeds.js
@@ -2,11 +2,11 @@ const Todo = require('../models/todo-model')
 
 const seedData = require('./todo-seeds.json')
 
-//Todo.deleteMany({})
-//.then(() => {
-//    return Todo.insertMany(seedData) //ideal for bulk insertion but skips schema validation
-//})
-Todo.insertMany(seedData) //ideal for bulk insertion but skips schema validation
+Todo.deleteMany({})
+.then(() => {
+   return Todo.insertMany(seedData) //ideal for bulk insertion but skips schema validation
+})
+//Todo.insertMany(seedData) //ideal for bulk insertion but skips schema validation
 .then(console.log)
 .catch(console.error)
 .finally(() => {process.exit()})
